df6e1dee671c6a9b5a3591327adb9a981ce6fd2f
clemsyn <clemsyn@yahoo.com>
2012-02-23 06:58:11 -0600

    Update arch/arm/mach-tegra/board-smba1002.h

--- arch/arm/mach-tegra/board-smba1002.h
+++ arch/arm/mach-tegra/board-smba1002.h
@@ -1,7 +1,7 @@
 /*
  * arch/arm/mach-tegra/board-smba1002.h
  *
- * Copyright (C) 2011 Eduardo Jos� Tagle <ejtagle@tutopia.com>
+ * Copyright (C) 2011 Eduardo José Tagle <ejtagle@tutopia.com>
  *
  * This software is licensed under the terms of the GNU General Public
  * License version 2, as published by the Free Software Foundation, and
@@ -17,7 +17,7 @@
 #ifndef _MACH_TEGRA_BOARD_SMBA1002_H
 #define _MACH_TEGRA_BOARD_SMBA1002_H
 
-#define TPS6586X_INT_BASE	TEGRA_NR_IRQS
+#define TPS6586X_INT_BASE  TEGRA_NR_IRQS
 
 #define SMBA1002_BT_RESET 		TEGRA_GPIO_PU0 	/* 0= reset asserted */
 
@@ -34,20 +34,20 @@
 /* #define SMBA1002_EMC_ELPIDA40NM	*/
 
 
-#define SMBA1002_CAMERA_POWER 	TEGRA_GPIO_PBB5 /* 1=powered on */
-#define SMBA1002_CAMERA_ROTATION	TEGRA_GPIO_PX7
+#define SMBA1002_CAMERA_POWER 	TEGRA_GPIO_PBB2 /* 1=powered on */
+//#define SMBA1002_CAMERA_ROTATION	TEGRA_GPIO_PX7
 
 #define SMBA1002_NAND_WPN		TEGRA_GPIO_PC7	/* NAND flash write protect: 0=writeprotected */
 
 #define SMBA1002_BL_ENB			TEGRA_GPIO_PD3
 #define SMBA1002_LVDS_SHUTDOWN	TEGRA_GPIO_PB2 // Is this right?
 #define SMBA1002_EN_VDD_PANEL	TEGRA_GPIO_PC6 
-#define SMBA1002_EN_VDIO_SDMMC  TEGRA_GPIO_PI6
-#define SMBA1002_EN_VDD_SDIO  TEGRA_GPIO_PT3 
+#define SMBA1002_EN_VDIO_SDMMC	TEGRA_GPIO_PI6
+#define SMBA1002_EN_VDD_SDIO	TEGRA_GPIO_PT3 
 #define SMBA1002_BL_VDD			TEGRA_GPIO_PW0
 #define SMBA1002_BL_PWM			TEGRA_GPIO_PU3 /* PWM */
-#define SMBA1002_HDMI_ENB		TEGRA_GPIO_PV5 /* unconfirmed */ // Does smba1002 have HDMI enbl?
-#define SMBA1002_HDMI_HPD		TEGRA_GPIO_P00 /* 1=HDMI plug detected */
+//#define SMBA1002_HDMI_ENB		TEGRA_GPIO_PV5 /* unconfirmed */ // Does smba1002 have HDMI enbl?
+#define SMBA1002_HDMI_HPD		TEGRA_GPIO_PO0 /* 1=HDMI plug detected */
 
 #define SMBA1002_BL_PWM_ID		0				/* PWM0 controls backlight */
 
@@ -58,9 +58,9 @@
 #define SMBA1002_MEM_SIZE 		SZ_512M			/* Total memory */
 #define SMBA1002_MEM_BANKS		1
 
-/*#define SMBA1002_GPU_MEM_SIZE 	SZ_128M*/		/* Memory reserved for GPU */
-/*#define SMBA1002_GPU_MEM_SIZE 	SZ_64M*/		/* Memory reserved for GPU */
-#define SMBA1002_GPU_MEM_SIZE 	(3*SZ_32M)		/* Memory reserved for GPU */
+//#define SMBA1002_GPU_MEM_SIZE 	SZ_128M		/* Memory reserved for GPU */
+//#define SMBA1002_GPU_MEM_SIZE 	SZ_64M		/* Memory reserved for GPU */
+#define SMBA1002_GPU_MEM_SIZE 	(3*SZ_32M)	/* Memory reserved for GPU */
 
 #define SMBA1002_FB1_MEM_SIZE 	(SZ_8M-SZ_2M)			/* Memory reserved for Framebuffer 1: LCD */
 #define SMBA1002_FB2_MEM_SIZE 	(SZ_8M-SZ_1M)			/* Memory reserved for Framebuffer 2: HDMI out */
@@ -90,19 +90,19 @@
 #define PMU_IRQ_BASE		(TEGRA_NR_IRQS)
 #define PMU_IRQ_RTC_ALM1 	(TPS6586X_INT_BASE + TPS6586X_INT_RTC_ALM1)
 
-#define	SMBA1002_ENABLE_VDD_VID	TEGRA_GPIO_PT	/* 1=enabled.  Powers HDMI. Wait 500uS to let it stabilize before returning */
+#define	SMBA1002_ENABLE_VDD_VID	TEGRA_GPIO_PT2	/* 1=enabled.  Powers HDMI. Wait 500uS to let it stabilize before returning */
 
 // TODO: Find whether there are any definitions for these?
 //#define SMBA1002_SDIO0_CD		TEGRA_GPIO_PI5
-#define SMBA1002_SDIO0_POWER	TEGRA_GPIO_PD0	/* SDIO0 and SDIO2 power */
+//#define SMBA1002_SDIO0_POWER	TEGRA_GPIO_PD0	/* SDIO0 and SDIO2 power */
 
 #define SMBA1002_SDHC_CD		TEGRA_GPIO_PI5
 #define SMBA1002_SDHC_WP		-1	/*1=Write Protected */
 #define SMBA1002_SDHC_POWER	TEGRA_GPIO_PD0
 
-#define SMBA1002_TS_IRQ		TEGRA_GPIO_PJ7 /*lets force board to use IRQ, see touch */
-#define SMBA1002_TS_RESET	TEGRA_GPIO_PQ7
-#define SMBA1002_TS_POWER	TEGRA_GPIO_PX0 /*need to look closer and put it far from Wifi and audio gpios*/
+#define SMBA1002_TS_IRQ		TEGRA_GPIO_PJ7
+#define SMBA1002_TS_RESET	TEGRA_GPIO_PH1
+#define SMBA1002_TS_POWER	TEGRA_GPIO_PK2
 //#define SMBA1002_TS_DISABLE	TEGRA_GPIO_PAA6 /* 0=enabled */
 
 //#define SMBA1002_FB_NONROTATE TEGRA_GPIO_PH1 /*1 = screen rotation locked */
@@ -113,12 +113,13 @@
 #define SMBA1002_BT_RST		TEGRA_GPIO_PU0
 
 #define SMBA1002_LOW_BATT	TEGRA_GPIO_PW3 /*(0=low battery)*/
-#define SMBA1002_IN_S3		TEGRA_GPIO_PAA7 /*1 = in S3 */
+//#define SMBA1002_IN_S3		TEGRA_GPIO_PAA4 /*1 = in S3 */
+#define TEGRA_GPIO_ISL29023_IRQ TEGRA_GPIO_PV5
 
-#define SMBA1002_USB0_VBUS		TEGRA_GPIO_PB0		/* 1= VBUS usb0 * maybe incorrect?*/
+#define SMBA1002_USB0_VBUS		TEGRA_GPIO_PB0		/* 1= VBUS usb0 */
 #define SMBA1002_USB1_RESET		TEGRA_GPIO_PV1	/* 0= reset */
 
-#define SMBA1002_HP_DETECT	TEGRA_GPIO_PW2 	/* HeadPhone detect for audio codec: 1=Hedphone plugged */
+#define SMBA1002_HP_DETECT	TEGRA_GPIO_PW2 	/* HeadPhone detect for audio codec: 1=Headphone plugged */
 
 #define SMBA1002_NVEC_REQ	TEGRA_GPIO_PD0	/* Set to 0 to send a command to the NVidia Embedded controller */
 #define SMBA1002_NVEC_I2C_ADDR 0x8a 			/* I2C address of Tegra, when acting as I2C slave */
@@ -170,9 +171,9 @@ extern int smba1002_camera_register_devices(void);
 
 #define TEGRA_ROUND_ALLOC(x) (((x) + 4095) & ((unsigned)(-4096)))
 /*Framebuffer Size for default Gtablet Panel*/
-#define SMBA1002_FB_SIZE TEGRA_ROUND_ALLOC(1024*600*(16/8)*SMBA1002_FB_PAGES)
+#define SMBA1002_FB_SIZE TEGRA_ROUND_ALLOC(1024*600*(32/8)*SMBA1002_FB_PAGES)
 /*Frambuffer size for 720p HDMI Framebuffer Output*/
-#define SMBA1002_FB_HDMI_SIZE TEGRA_ROUND_ALLOC(1280*720*(32/8)*2)
+#define SMBA1002_FB_HDMI_SIZE TEGRA_ROUND_ALLOC(1920*1080*(32/8)*2)
 
 
 #endif
